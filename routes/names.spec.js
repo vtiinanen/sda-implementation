9800208b662e8022888a76fbc904a1eb180462c9
vtiinanen <49340148+vtiinanen@users.noreply.github.com>
2021-01-21 15:52:30 +0200

    Change toEqual into toBe

--- routes/names.spec.js
+++ routes/names.spec.js
@@ -12,47 +12,47 @@ describe('names routes tests', () => {
 	describe('GET /names', () => {
 		test('should return names', async () => {
 			const { statusCode, body } = await api.get('/names').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
 		});
 		test('should return names when queried with sort', async () => {
 			const { statusCode, body } = await api.get('/names?sort').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
 		});
 		test('should return names when queried with sort=amount', async () => {
 			const { statusCode, body } = await api.get('/names?sort=amount').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
 		});
 		test('should return names when queried with sort=alphabetical', async () => {
 			const { statusCode, body } = await api.get('/names?sort=alphabetical').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
 		});
 		test('should return names when queried with aggregate=amount', async () => {
 			const { statusCode, body } = await api.get('/names?aggregate=amount').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
-			expect(body).toEqual(211);
+			expect(body).toBe(211);
 		});
 	});
 
 	describe('GET /names/:name', () => {
 		test('should return name document', async () => {
 			const { statusCode, body } = await api.get('/names/Ville').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
 		});
 		test('should return name documents key when queried with key=amount', async () => {
 			const { statusCode, body } = await api.get('/names/Ville?key=amount').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(200);
+			expect(statusCode).toBe(200);
 			expect(body).toBeDefined();
-			expect(body).toEqual(24);
+			expect(body).toBe(24);
 		});
 		test('should return 404 when requestin non-existing name object', async () => {
 			const { statusCode, body } = await api.get('/names/IdontExistHAHA').expect('Content-Type', /json/);
-			expect(statusCode).toEqual(404);
+			expect(statusCode).toBe(404);
 			expect(body).toBeDefined();
 		});
 	});
